e4ab09d77936fcdcf2c5630b668b89251a80ea5d
Desmond Young <Desmond.Young25@yahoo.com>
2020-06-02 10:22:25 -0700

    Wrote code to the Deal button and both left and right card to function and shuffle.

--- WarCardApp/ViewController.swift
+++ WarCardApp/ViewController.swift
@@ -21,11 +21,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view
-    }
+   }
     
     @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftNumber = Int.random(in: 2...14)
+        
+        let rightNumber = Int.random(in: 2...14)
   
-        print("Deal Tapped!")
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
     }
 }
 
